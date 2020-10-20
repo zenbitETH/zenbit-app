6e1d2642a70b762de2a6dd7b77f68447551dfc9a
Mario Briseño <mario.briseno7@gmail.com>
2020-10-19 20:59:08 -0500

    Updates

---
+++ precache-manifest.cc5d3fdd829feca2344b7ecb0b3a3ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42693578036e680c555cd50d783bf6d",
+    "url": "/zenbit-app/index.html"
+  },
+  {
+    "revision": "5672e468177bb94e2ab3",
+    "url": "/zenbit-app/static/css/main.a98b7390.chunk.css"
+  },
+  {
+    "revision": "b5cc18c07080c1f75760",
+    "url": "/zenbit-app/static/js/2.03b55525.chunk.js"
+  },
+  {
+    "revision": "5c5a0857bb8747e9ae7db7917ecbbf79",
+    "url": "/zenbit-app/static/js/2.03b55525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5672e468177bb94e2ab3",
+    "url": "/zenbit-app/static/js/main.4c8464a1.chunk.js"
+  },
+  {
+    "revision": "196457cedeb1680b1ffc",
+    "url": "/zenbit-app/static/js/runtime-main.6fe1c3ff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zenbit-app/precache-manifest.c437e9d419e1a28c209ba1e84d0a340b.js"
+  "/zenbit-app/precache-manifest.cc5d3fdd829feca2344b7ecb0b3a3ab7.js"
 );
 
 self.addEventListener('message', (event) => {
