a01a543a5bed6d2c3b6b1ea8c9718115ebe5a97d
Mario Briseño <mario.briseno7@gmail.com>
2020-10-20 19:49:46 -0500

    component just to fill the space, not doing much

--- src/components/HeadContent.js
+++ src/components/HeadContent.js
@@ -4,9 +4,9 @@ import React from "react"
 function HeadContent() {
     return(
         <div>
-            <p>
-                Head Content
-            </p>
+            <h2 className="head-content">
+                Es peligroso no tener habilidades digitales.
+            </h2>
         </div>
     )
 }
