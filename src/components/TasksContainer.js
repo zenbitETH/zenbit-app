4dcef5582d0bbf2542459e59270ebc5e5cbb3475
Mario Briseño <mario.briseno7@gmail.com>
2020-10-22 13:14:24 -0500

    adding button to handle connection to metamask

--- src/components/TasksContainer.js
+++ src/components/TasksContainer.js
@@ -5,22 +5,31 @@ import KeysManagement from "./contract_tasks/KeysManagement"
 import AssetsManagement from "./contract_tasks/AssetsManagement"
 import RegisterView from "./contract_tasks/RegisterView"
 
-let web3
+let web3;
+let accounts;
 
 class TasksContainer extends React.Component {
 
     constructor() {
         super()
         this.state = {
-
+            buttonVisibility: "none"
         }
         this.initComponent = this.initComponent.bind(this)
+        //this.initComponent();
+        this.enableMetamask = this.enableMetamask.bind(this)
         this.initComponent()
     }
 
     render() {
         return(
             <div>
+                <button type="submit"
+                style={{display: this.state.buttonVisibility}}
+                onClick={this.enableMetamask}
+                className="button-submit">
+                    Conectar Metamask
+                </button>
                 <SignInComponent />
                 <KeysManagement />
                 <AssetsManagement />
@@ -28,16 +37,9 @@ class TasksContainer extends React.Component {
             </div>
     )}
 
-    initComponent() {
+    async initComponent() {
         if (window.ethereum) {
             web3 = new Web3(window.ethereum);
-            try { 
-                window.ethereum.enable().then(function() {
-                // User has allowed account access to DApp...
-                });
-            } catch(e) {
-                // User has denied account access to DApp...
-            }
        }
        // Legacy DApp Browsers
         else if (window.web3) {
@@ -47,6 +49,28 @@ class TasksContainer extends React.Component {
         else {
             alert('You have to install MetaMask !');
         }
+
+        if(typeof web3 === "undefined") {
+            return 0
+        }
+
+        accounts = await web3.eth.getAccounts()
+        if(accounts.length === 0) {
+            this.setButtonVisibility("initial")
+        } else {
+            this.setButtonVisibility("none")
+        }
+    }
+
+    async enableMetamask() {
+        accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        this.setButtonVisibility("none")
+    }
+
+    setButtonVisibility(isVisible) {
+        this.setState({
+            buttonVisibility: isVisible
+        })
     }
 
 }
