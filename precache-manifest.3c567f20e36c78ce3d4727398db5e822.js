87cc3996e2d4bf42ce578cfcced17703a91d2d06
Mario Briseño <mario.briseno7@gmail.com>
2020-10-22 12:52:56 -0500

    Updates

---
+++ precache-manifest.3c567f20e36c78ce3d4727398db5e822.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1504b8f3700f8b82c7f88055df1f363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8cea9186ac895eece52",
+    "url": "/static/css/main.49e45c83.chunk.css"
+  },
+  {
+    "revision": "384c25af95f026a6d786",
+    "url": "/static/js/2.d3f54541.chunk.js"
+  },
+  {
+    "revision": "5c5a0857bb8747e9ae7db7917ecbbf79",
+    "url": "/static/js/2.d3f54541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cea9186ac895eece52",
+    "url": "/static/js/main.15f02894.chunk.js"
+  },
+  {
+    "revision": "e7652b68a880867001bb",
+    "url": "/static/js/runtime-main.fbfeaf1d.js"
+  },
+  {
+    "revision": "7d7e9dd7c06a1c0123ddce742b4a6a3d",
+    "url": "/static/media/zl.7d7e9dd7.png"
+  }
+]);
